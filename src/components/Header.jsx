3a2a30c42f6222d59502935662464ebbca0f8e25
Hari-Prasanth3 <harihari71775@email.com>
2024-02-04 20:56:53 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import {Navbar} from '@material-tailwind/react'
 
 const Header = () => {
   return (
-  <Navbar className='   p-6 backdrop-blur lg:px-8 bg-red-400 border-none ' >
+  <Navbar className='   p-6 backdrop-blur lg:px-8 bg-red-400 border-none  rounded-none' >
 <p class='text-center items-center text-black-800 text-4xl font-serif font-bold'>    Dictionary
 </p>  </Navbar>
   )
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -53,6 +53,7 @@ const Search = () => {
   console.log(Error);
 
   return (
+    <>
     <div>
       {!meaning && !Error ? (
         <div className="flex flex-col items-center bg-white mt-16 text-base">
@@ -76,13 +77,13 @@ const Search = () => {
             <p className="text-left mt-1 items-center">
               {" "}
               <br />
-              Type a word and press enter to get meaning pronounication,
+              Type a word and press enter to get meaning Pronunciation,
               <br />
               examples, synonyms, and more
             </p>{" "}
             <br />
           </FormControl>
-          <img className="" width={350} src={image} /> <br />
+          <img className="h-80" width={350} src={image} /> <br />
           <p className="text-sm">Seems kind of empty here!</p>
         </div>
       ) : !meaning && Error ? (
@@ -118,49 +119,51 @@ const Search = () => {
               ></TextField>
               <Button
                 className="text-sm font-semibold text-white bg-blue-600 rounded shadow-md border-2 border-blue-600 md:text-base hover:bg-white hover:text-blue-900 mt-2"
-                onClick={onEnterKeyDown}
-              >
+                onClick={onEnterKeyDown}>
                 Search
               </Button>
+          
             </FormControl>
-            <div className="flex flex-col mt-10 justify-center items-center md:flex-row shadow rounded-xl bg-violet-200  m-2">
-              <div className="p-10 mb-auto">
-                {<h2>Word: {results[0]?.word}</h2>}
+            <div/>
+             <div className="flex flex-cols-4 mt-10 justify-center items-center md:flex-row shadow rounded-xl bg-violet-200  m-2 pl-20" >
+              <div className="mb-auto mt-10 p-5">
+                {<h2>Word </h2>}
+                <ul>{results[0]?.word}</ul>
               </div>
 
-              <div className="flex flex-col p-10 mb-auto justify-center mr-10  leading-8">
-                <div className="">
+              <div className="mb-auto mt-10 p-5">
+               
                   {results && (
-                    <div className="pr-10">
-                      <p>Pronunciation: </p>
-                      <p>
+                  
+                    
+                      <p>Pronunciation
                         {results[0]?.phonetics.map((x) => {
                           return <li> {x.text}</li>;
                         })}
                       </p>
-                    </div>
+                 
                   )}
-                </div>
+              
               </div>
 
-              <div className="flex flex-col  justify-center mb-auto pt-10  ">
-                <div className="heading">
+              <div className="mb-auto mt-10 p-5  ">
+               
                   {results && <h2>Part Of Speech</h2>}
-                </div>
+               
 
-                <div className="ul">
+            
                   {results[0].meanings.map((x, index) => {
                     return <li key={index}> {x.partOfSpeech}</li>;
                   })}
-                </div>
+              
               </div>
 
-              <div className="flex flex-row justify-center p-10 mb-auto leading-8">
-                <div className="heading">
-                  {results && <h2>Definitions:</h2>}
-                </div>
+              <div className="mb-auto mt-10 p-5 pl-5">
+                
+                  {results && <h2>Definitions</h2>}
+               
 
-                <div className="ul">
+           
                   {/*how to use map function to fetch from array */}
                   <ul>
                     {results[0].meanings.map((x, index) => {
@@ -169,14 +172,14 @@ const Search = () => {
                       });
                     })}
                   </ul>
-                </div>
+              
               </div>
             </div>
-          </div>
+        </div>
         )
       )}
-    </div>
-  );
+    </div></>
+  )
 };
 
 export default Search;
